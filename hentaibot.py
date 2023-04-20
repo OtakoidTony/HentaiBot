7edc3b457a047cc22c2507391e641445ef44d8b3
Gary Jordaan <82768608+Varien-1936@users.noreply.github.com>
2023-04-20 12:34:53 +0200

    Moved the discordpy version files to main folder

--- hentaibot.py
+++ hentaibot.py
@@ -4,24 +4,26 @@ import requests
 import random
 import nekos_fun
 import discord
-from discord.ext import commands
 from discord import app_commands
 from typing import Optional, Literal
 
 
 class Bot(discord.Client):
-    def __init__(self, *, intents: discord.Intents):
-        super().__init__(intents=intents)
+    def __init__(self):
+        super().__init__()
         self.tree = app_commands.CommandTree(self)
+        self.tree.on_error = self.on_app_command_error
+
     async def setup_hook(self):
         await self.tree.sync()
 
 
-intents=discord.Intents(messages = True, members = True, typing = True, guilds = True)
+intents = discord.Intents(messages=True, members=True, typing=True, guilds=True)
 bot = Bot(intents=intents)
 game = discord.Game("Naughty")
 
 token = os.environ["TOKEN"]
+
 
 @bot.event
 async def on_ready():
@@ -30,7 +32,7 @@ async def on_ready():
 
 
 @bot.tree.command(description="Shows the help menu", nsfw=True)
-async def help(ctx:discord.Interaction):
+async def help(ctx: discord.Interaction):
     await ctx.response.defer()
     ret = "```\n"
     ret += "gelbooru [tags]\n"
@@ -48,31 +50,35 @@ async def help(ctx:discord.Interaction):
     ret += "```"
     await ctx.followup.send(ret)
 
+
 @bot.tree.command(description="Get hentai from Gelbooru", nsfw=True)
 @app_commands.describe(tag="Add a tag")
-async def gelbooru(ctx:discord.Interaction, tag:Optional[str]=None)->None:
+async def gelbooru(ctx: discord.Interaction, tag: Optional[str] = None) -> None:
     await ctx.response.defer()
     formated_tag = tag.replace(" ", "_")
     if tag == None:
-        api="https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub"
-        response = requests.get(api)
-        ret = json.loads(response.text)
-        post = random.choice(ret["post"])
+        api = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub"
+
     else:
-        api="https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub" + formated_tag
-        response = requests.get(api)
-        ret = json.loads(response.text)
-        post = random.choice(ret["post"])
+        api = (
+            "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&limit=100&tags=rating:explicit+-loli+-shota+-cub"
+            + formated_tag
+        )
+    response = requests.get(api)
+    ret = json.loads(response.text)
+    post = random.choice(ret["post"])
     source = post["source"]
     owner = post["owner"]
     score = post["score"]
-    image:str=post["file_url"]
-    created_at=post["created_at"]
+    image: str = post["file_url"]
+    created_at = post["created_at"]
     if image.endswith(".mp4"):
         await ctx.followup.send(image)
     else:
         embed = discord.Embed(title="Created by {}".format(owner))
-        embed.add_field(name="Source", value="[Click here]({})".format(source), inline=True)
+        embed.add_field(
+            name="Source", value="[Click here]({})".format(source), inline=True
+        )
         embed.add_field(name="Score", value=score, inline=True)
         embed.set_image(url=image)
         embed.set_footer(text="Fetched from Gelbooru\nCreated at {}".format(created_at))
@@ -81,20 +87,27 @@ async def gelbooru(ctx:discord.Interaction, tag:Optional[str]=None)->None:
 
 @bot.tree.command(description="Get hentai from Yandere", nsfw=True)
 @app_commands.describe(tag="Add a tag")
-async def yandere(ctx: discord.Interaction, tag:Optional[str]=None)->None:
+async def yandere(ctx: discord.Interaction, tag: Optional[str] = None) -> None:
     await ctx.response.defer()
     if tag == None:
-        ret = random.choice(requests.get(
-            "https://yande.re/post.json?limit=100&tags=rating:explicit+-loli+-shota+-cub").json())
+        ret = random.choice(
+            requests.get(
+                "https://yande.re/post.json?limit=100&shown:true&tags=rating:explicit+-loli+-shota+-cub"
+            ).json()
+        )
     else:
         tag = tag.replace(" ", "_")
-        ret = random.choice(requests.get(
-            "https://yande.re/post.json?limit=100&tags=rating:explicit+-loli+-shota+-cub" + tag).json())
+        ret = random.choice(
+            requests.get(
+                "https://yande.re/post.json?limit=100&shown:true&tags=rating:explicit+-loli+-shota+-cub"
+                + tag
+            ).json()
+        )
     created_at = ret["created_at"]
-    file=ret["file_url"]
-    author=ret["author"]
-    source=ret["source"]
-    score=ret["score"]
+    file = ret["file_url"]
+    author = ret["author"]
+    source = ret["source"]
+    score = ret["score"]
     embed = discord.Embed(title="Created by {}".format(author))
     embed.add_field(name="Source", value="[Click here]({})".format(source), inline=True)
     embed.add_field(name="Score", value=score, inline=True)
@@ -105,14 +118,23 @@ async def yandere(ctx: discord.Interaction, tag:Optional[str]=None)->None:
 
 @bot.tree.command(description="Get hentai from Konachan", nsfw=True)
 @app_commands.describe(tag="Add a tag")
-async def konachan(ctx:discord.Interaction, tag:Optional[str]=None)->None:
+async def konachan(ctx: discord.Interaction, tag: Optional[str] = None) -> None:
     await ctx.response.defer()
     if tag == None:
-        ret = random.choice(requests.get("https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub").json())
+        ret = random.choice(
+            requests.get(
+                "https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub"
+            ).json()
+        )
 
     else:
         tag = tag.replace(" ", "_")
-        ret = random.choice(requests.get(f"https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub" + tag).json())
+        ret = random.choice(
+            requests.get(
+                f"https://konachan.com/post.json?s=post&q=index&limit=100&tags=rating:explicit+-loli+-shota+-cub"
+                + tag
+            ).json()
+        )
 
     created_at = ret["created_at"]
     file = ret["file_url"]
@@ -120,27 +142,34 @@ async def konachan(ctx:discord.Interaction, tag:Optional[str]=None)->None:
     source = ret["source"]
     score = ret["score"]
     embed = discord.Embed(title="Created by {}".format(author))
-    embed.add_field(
-        name="Source", value="[Click here]({})".format(source), inline=True)
+    embed.add_field(name="Source", value="[Click here]({})".format(source), inline=True)
     embed.add_field(name="Score", value=score, inline=True)
     embed.set_image(url=file)
-    embed.set_footer(
-        text="Fetched from Konachan\nCreated at {}".format(created_at))
+    embed.set_footer(text="Fetched from Konachan\nCreated at {}".format(created_at))
     await ctx.followup.send(embed=embed)
 
 
 @bot.tree.command(description="Get hentai from Danbooru", nsfw=True)
 @app_commands.describe(tag="Add a tag")
-async def danbooru(ctx: discord.Interaction, tag:Optional[str]=None)->None:
+async def danbooru(ctx: discord.Interaction, tag: Optional[str] = None) -> None:
     await ctx.response.defer()
     if tag == None:
-        ret = random.choice(requests.get(
-            "https://danbooru.donmai.us/posts.json?limit=100&tags=rating:explicit-loli-shota-cub").json())
+        ret = random.choice(
+            requests.get(
+                "https://danbooru.donmai.us/posts.json?limit=100&tags=rating:explicit-loli-shota-cub",
+                headers="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
+            ).json()
+        )
 
     else:
         tag = tag.replace(" ", "_")
-        ret = random.choice(requests.get(
-            "https://danbooru.donmai.us/posts.json?limit=100&tags=rating:explicit-loli-shota-cub" + tag).json())
+        ret = random.choice(
+            requests.get(
+                "https://danbooru.donmai.us/posts.json?limit=100&tags=rating:explicit-loli-shota-cub"
+                + tag,
+                headers="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/37.0.2062.94 Chrome/37.0.2062.94 Safari/537.36",
+            ).json()
+        )
 
     created_at = ret["created_at"]
     file = ret["file_url"]
@@ -148,62 +177,79 @@ async def danbooru(ctx: discord.Interaction, tag:Optional[str]=None)->None:
     source = ret["source"]
     score = ret["score"]
     embed = discord.Embed(title="Created by {}".format(author))
-    embed.add_field(
-        name="Source", value="[Click here]({})".format(source), inline=True)
+    embed.add_field(name="Source", value="[Click here]({})".format(source), inline=True)
     embed.add_field(name="Score", value=score, inline=True)
     embed.set_image(url=file)
-    embed.set_footer(
-        text="Fetched from Danbooru\nCreated at {}".format(created_at))
+    embed.set_footer(text="Fetched from Danbooru\nCreated at {}".format(created_at))
     await ctx.followup.send(embed=embed)
 
 
 @bot.tree.command(description="Get lewded nekos from NekoLove", nsfw=True)
-async def nekolewd(ctx:discord.Interaction):
+async def nekolewd(ctx: discord.Interaction):
     await ctx.response.defer()
     ret = requests.get("https://neko-love.xyz/api/v1/nekolewd").json()
-    response=ret["code"]
-    image=ret["url"]
+    response = ret["code"]
+    image = ret["url"]
 
-    if response !=200:
-        embed = discord.Embed(description="Error with API!\nPlease contact the neko-love.xyz team. If the fault is not on their side, please reach up to me")
+    if response != 200:
+        embed = discord.Embed(
+            description="Error with API!\nPlease contact the neko-love.xyz team. If the fault is not on their side, please reach up to me"
+        )
         await ctx.followup.send(embed=embed)
     else:
         embed = discord.Embed()
         embed.set_image(url=image)
-        embed.set_footer(
-            text="Fetched from NekoLove")
+        embed.set_footer(text="Fetched from NekoLove")
         await ctx.followup.send(embed=embed)
 
 
 @bot.tree.command(description="Get an image/gif from NekosLife", nsfw=True)
 @app_commands.describe(tag="Which tag?")
-async def nekosfun(ctx:discord.Interaction, tag:Optional[Literal["ass", "bj", "boobs", "cum", "hentai", "spank", "gasm", "lesbian", "pussy"]]):
+async def nekosfun(
+    ctx: discord.Interaction,
+    tag: Optional[
+        Literal[
+            "ass", "bj", "boobs", "cum", "hentai", "spank", "gasm", "lesbian", "pussy"
+        ]
+    ],
+):
     if tag == None:
-        tags = ["ass", "bj", "boobs", "cum", "hentai",
-                "spank", "gasm", "lesbian", "pussy"]
+        tags = [
+            "ass",
+            "bj",
+            "boobs",
+            "cum",
+            "hentai",
+            "spank",
+            "gasm",
+            "lesbian",
+            "pussy",
+        ]
         image = nekos_fun.nekofun(random.choice(tags))
-    
+
     else:
-        image = nekos_fun.nekofun(str(tag))
+        image = nekos_fun.nekofun(tag)
 
     if image != 200:
         await ctx.followup.send(image)
     else:
         embed = discord.Embed()
         embed.set_image(url=image)
-        embed.set_footer(
-            text="Fetched from Nekos.Fun")
+        embed.set_footer(text="Fetched from Nekos.Fun")
         await ctx.followup.send(embed=embed)
 
-@bot.tree.command(description="Ask a member if you can f*ck them")
-@app_commands.describe(member="Which member?")
-async def f_ck(ctx:discord.Integration, member:Member)
 
 @bot.event
-async def on_application_command_error(ctx:discord.Interaction, error:app_commands.AppCommandError):
+async def on_app_command_error(
+    ctx: discord.Interaction, error: app_commands.AppCommandError
+):
     if isinstance(error, KeyError):
-        embed = discord.Embed(title="Hentai Failed", description="Hentai couldn't be sent in this channel",
-            color=0xff0000).add_field(name="Reason", value="Tag not found")
+        embed = discord.Embed(
+            title="Hentai Failed",
+            description="Hentai couldn't be sent in this channel",
+            color=0xFF0000,
+        ).add_field(name="Reason", value="Tag not found")
         await ctx.followup.send(embed=embed)
+
 
 bot.run(token)
